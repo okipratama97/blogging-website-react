2f34198538cebe586d49fd1503358d28f87439a3
Helvizar Muhammad <helvizaranderson@gmail.com>
2023-06-24 17:43:54 +0700

    add register logic

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -1,9 +1,12 @@
+
 // Register form
 // Using Formik and Yup validator for input user data
 
 import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
+import { useNavigate } from 'react-router-dom';
+import axios, * as others from 'axios';
 
 // Validation schema using Yup
 const validationSchema = Yup.object().shape({
@@ -13,17 +16,39 @@ const validationSchema = Yup.object().shape({
     .min(6, 'Password must be at least 6 characters')
     .matches(/^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/, 'Password must contain at least one uppercase letter, one number, and one symbol')
     .required('Password is required'),
+    confirmPassword: Yup.string()
+    .min(6, 'Password must be at least 6 characters')
+    .matches(/^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/, 'Password must contain at least one uppercase letter, one number, and one symbol')
+    .required('Confirm Password is required'),
   phoneNumber: Yup.string().required('Phone number is required'),
 });
 
 const RegisterForm = () => {
   // Handle form submission
-  const onSubmit = (values, { setSubmitting }) => {
-    // Perform registration logic, e.g., send data to the server
-    console.log(values);
-
-    // Reset form fields and set submitting state
-    setSubmitting(false);
+  const navigate = useNavigate();
+  const onRegister = async (values) => {
+    try {
+      const username = values.username
+      const email = values.email
+      const password = values.password
+      const phone = values.phoneNumber
+      const confirmPassword = values.confirmPassword;
+      const FE_URL = process.env.FE_URL;
+      console.log(username, email, password, phone, confirmPassword);
+      await axios.post("https://minpro-blog.purwadhikabootcamp.com/api/auth/", {
+        username,
+        email,
+        password,
+        phone,
+        confirmPassword,
+        FE_URL
+      });
+      setTimeout(() => {
+        navigate("/login");
+      }, 2000);
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
@@ -34,10 +59,11 @@ const RegisterForm = () => {
           username: '',
           email: '',
           password: '',
-          phoneNumber: '',
+          phone: '',
+          confirmPassword: ''
         }}
         validationSchema={validationSchema}
-        onSubmit={onSubmit}
+        onSubmit={onRegister}
       >
         <Form className='text-green'>
           <div>
@@ -59,6 +85,11 @@ const RegisterForm = () => {
             <label htmlFor="phoneNumber">Phone Number</label>
             <Field type="text" name="phoneNumber" />
             <ErrorMessage name="phoneNumber" component="div" />
+          </div>
+          <div>
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <Field type="password" name="confirmPassword" />
+            <ErrorMessage name="confirmPassword" component="div" />
           </div>
           <div>
             <button type="submit">Register</button>
