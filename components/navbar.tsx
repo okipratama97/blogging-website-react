59789134d6d3d2a196631bfa632145a40c01f426
Helvizar Muhammad <helvizaranderson@gmail.com>
2023-08-07 17:10:21 +0700

    modify navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,10 +2,14 @@ import Link from "next/link";
 
 import Container from "@/components/ui/container";
 import MainNav from "@/components/main-nav";
+import NavbarActions from "@/components/navbar-actions";
 import getCategories from "@/actions/get-categories";
 
+export const revalidate = 0;
+
 const Navbar = async () => {
-  const categories = getCategories();
+  // const categories = getCategories(); // buat ngeget category dari cms
+
   return ( 
     <div className="border-b">
       <Container>
@@ -13,7 +17,8 @@ const Navbar = async () => {
           <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
             <p className="font-bold text-xl">STOCKSHOP</p>
           </Link>
-          <MainNav data={[]}/>
+          {/* <MainNav data={[]}/> to render routes */} <div className="mx-5"> <p>Categories</p> </div>
+          <NavbarActions />
         </div>
       </Container>
     </div>
